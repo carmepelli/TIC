80a17cd20abcd98240fbb8cab14aeda0dd13c8d1
carmepelli <93370924+carmepelli@users.noreply.github.com>
2021-12-20 10:42:38 +0100

    Add files via upload

---
+++ exercici4.5.py
@@ -0,0 +1,7 @@
+print("Este programa recibe tres números y devuelve la suma.")
+total = 0
+ 
+for i in range(3):
+    x = int(input("Introduce un número: "))
+    total = total + x 
+print("El total es:", total)
